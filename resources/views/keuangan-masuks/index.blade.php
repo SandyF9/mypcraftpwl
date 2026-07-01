785a45f9896767d3af45b48e4f557c58a65c3edd
SandyF9 <akhiongsandy88@gmail.com>
2026-07-01 15:08:03 +0700

    Baru

--- resources/views/keuangan-masuks/index.blade.php
+++ resources/views/keuangan-masuks/index.blade.php
@@ -5,7 +5,7 @@
                 {{ __('Daftar Keuangan Masuk') }}
             </h2>
             <a href="{{ route('keuangan-masuks.create') }}" 
-                class="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition font-medium text-sm">
+                class="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700 transition font-medium text-sm">
                 Tambah Data
             </a>
         </div>
